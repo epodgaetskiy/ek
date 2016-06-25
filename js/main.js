d66808f9763eb676bb7a4e704132c05db9a294ee
Evgeniy Podgaetskiy <epodgaetskiy@gmail.com>
2016-06-25 13:06:04 +0300

    fix new

--- js/main.js
+++ js/main.js
@@ -9,4 +9,25 @@ $(document).ready(function(){
 		$('.navbar--link').removeClass('navbar-active');
 		$(this).addClass('navbar-active');
 	})
+
+	$('.get_subscription').on('click', function(){
+		$(this).prop('checked', false);
+		$('.main__getUpdates--container').hide();
+		$('.subscribe').show("slow")
+	});
+
+	$('.subscribe-close').on('click', function(){
+		$('.subscribe').hide("slow");
+		$('.main__getUpdates--container').show();
+	})
+
+	$('.mobile-menu').click(function(){
+		$('.main__content--nav').show("slow")
+	})
+
+	$('.mobile-close').click(function(){
+		$('.main__content--nav').hide("slow")
+	})
 })
+
+
